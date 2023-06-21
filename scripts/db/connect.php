0f816cb9da4061146f1bc0f551192888d3b8ea32
emilynievesb <nemily1710@gmail.com>
2023-06-21 14:45:35 -0500

    feat: :sparkles: Conexión a la base de datos
    Se creó la conexión a la base de datos con PDO, y se asignaron sus respectivas credenciales

---
+++ scripts/db/connect.php
@@ -0,0 +1,28 @@
+<?php
+
+interface enviroments
+{
+    public function __get($name);
+}
+
+// Al testear la conexión, al ser una clase abstracta, no se imprime el echo. Y para testearla es necesario
+// instanciarla con new pq no usa getInstance, y pues hay que quitar el abstract
+abstract class connect extends credentials implements enviroments
+{
+    // private $pass = "";
+    // private $user = "root";
+    protected $conx;
+    function __construct(private $driver = 'mysql', private $port = 3306)
+    {
+        try {
+            $this->conx = new PDO($this->driver . ":host=" . $this->__get('host') . ";port=" . $this->port . "; dbname=" . $this->__get('dbname') . "; user=" . $this->user . "; password=" . $this->password);
+            $this->conx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "ok";
+        } catch (PDOException $error) {
+            echo "No me pude conectar" . $error->getMessage();
+        }
+    }
+
+}
+
+?>
---
+++ scripts/db/credentials.php
@@ -0,0 +1,21 @@
+<?php
+// Si hay un atributo privado, solo se puede usar en la clase donde está, la herencia no puede acceder a él.
+abstract class credentials
+{
+    // protected $host = 'localhost';
+    protected $host = '172.16.48.204';
+
+    private $user = 'sputnik';
+    // private $user = 'root';
+    private $password = 'Sp3tn1kC@';
+    // private $password = '';
+
+    protected $dbname = 'campusland';
+
+    public function __get($name)
+    {
+        return $this->{$name};
+    }
+}
+
+?>
