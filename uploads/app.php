e3f9a15a391f646400eece5faae6400f16dde16e
emilynievesb <nemily1710@gmail.com>
2023-06-21 14:49:24 -0500

    feat: :sparkles: Se creó el autoload
    Se creó el autoload que a su vez usa singleton para instanciar

---
+++ uploads/app.php
@@ -0,0 +1,50 @@
+<?php
+
+
+trait getInstance
+{
+    public static $instance;
+    public static function getInstance()
+    {
+        $arg = func_get_args();
+        $arg = array_pop($arg);
+        return (!(self::$instance instanceof self) || !empty($arg)) ? self::$instance = new static(...(array) $arg) : self::$instance;
+    }
+    //*Set y get mágicos...
+    function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+}
+
+function autoload($class)
+{
+    //Directorios donde buscar archivod de clases
+    $directories = array();
+    $directorio = dirname(__DIR__) . '/scripts';
+    $elementos = scandir($directorio);
+    foreach ($elementos as $elemento) {
+        $rutaElemento = $directorio . '/' . $elemento . '/';
+        if (is_dir($rutaElemento) && $elemento !== '.' && $elemento !== '..') {
+            $directories[] = $rutaElemento;
+        }
+    }
+    //Convertir el nombre de la clase en un nombre de archivo relativo
+    $classFile = str_replace('\\', '/', $class) . '.php';
+
+    //Recorre los directorios y buscar el archivo de la clase
+    foreach ($directories as $directory) {
+        $file = $directory . $classFile;
+        //Verificar si el archivo existe y cargarlo
+        if (file_exists($file)) {
+            require $file; //!El require solo funciona una vez
+            return; //!El break no funciona muy bien
+        }
+    }
+}
+spl_autoload_register('autoload');
+animal::getInstance();
+
+
+?>
