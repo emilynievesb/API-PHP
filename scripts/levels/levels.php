69dae180a4852f37dc9984c55970ccaa1ddec5fe
emilynievesb <nemily1710@gmail.com>
2023-06-22 21:05:43 -0500

    feat: :sparkles: Clase Levels
    Se creó el respectivo crud base de la clase y se asignaron los atributos para el llenado de la base de datos

---
+++ scripts/levels/levels.php
@@ -0,0 +1,99 @@
+<?php
+class levels extends connect
+{
+    private $queryPost = 'INSERT INTO levels (name_level, group_level) VALUES (:name, :group)';
+    private $queryPut = 'UPDATE levels SET name_level = :name, group_level = :group WHERE  id = :id';
+    private $queryGetAll = 'SELECT  id AS "id", name_level AS "name", group_level AS "group" FROM levels';
+    private $queryDelete = 'DELETE FROM levels WHERE id = :id';
+    private $message;
+
+    use getInstance;
+    //*Se definen el tipo de dato: static, private, public
+    function __construct(public $id = 1, public $name_level = 1, public $group_level)
+    {
+        parent::__construct();
+
+    }
+    public function postCountries()
+    {
+        /*Prepare es literalmente preparar el query */
+        $res = $this->conx->prepare($this->queryPost);
+        /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
+        try {
+            /**El bindValue le asigna valores al alias que puse en el queryPost */
+            $res->bindValue("name", $this->name_level);
+            $res->bindValue("group", $this->group_level);
+            /**Execute es para ejecutar */
+            $res->execute();
+            $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Inserted data", "res" => $res];
+        } catch (\PDOException $e) {
+            /**Message es un array asociativo */
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+
+    public function updateCountries()
+    {
+        /*Prepare es literalmente preparar el query */
+        $res = $this->conx->prepare($this->queryPut);
+        /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
+        try {
+            /**El bindValue le asigna valores al alias que puse en el queryPut */
+            $res->bindValue("id", $this->id);
+            $res->bindValue("name", $this->name_level);
+            $res->bindValue("group", $this->group_level);
+            /**Execute es para ejecutar */
+            $res->execute();
+            if ($res->rowCount() > 0) {
+                $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Data updated"];
+            } else {
+                $this->message = ["Code" => 404, "Message" => "Data not founded"];
+
+            }
+        } catch (\PDOException $e) {
+            /**Message es un array asociativo */
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+
+    public function deleteCountries()
+    {
+        /*Prepare es literalmente preparar el query */
+        $res = $this->conx->prepare($this->queryDelete);
+        /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
+        try {
+            $res->bindValue("id", $this->id);
+            /**Execute es para ejecutar */
+            $res->execute();
+
+            if ($res->rowCount() > 0) {
+                $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Data deleted"];
+            } else {
+                $this->message = ["Code" => 404, "Message" => "Data not founded"];
+            }
+        } catch (\PDOException $e) {
+            /**Message es un array asociativo */
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+
+    public function getAllCountries()
+    {
+        try {
+            $res = $this->conx->prepare($this->queryGetAll);
+            $res->execute();
+            $this->message = ["Code" => 200, "Message" => $res->fetchAll(PDO::FETCH_ASSOC)];
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            print_r($this->message);
+        }
+    }
+}
+?>
