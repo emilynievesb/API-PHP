b02ef227aa3139c0c8ff29555c29e2419c139bdb
emilynievesb <nemily1710@gmail.com>
2023-06-26 09:23:52 -0500

    feat: :sparkles: CRUD  emergency_contact
    Se creó el respectivo CRUD de la clase, con sus respectivos inner join

--- scripts/emergency_contact/emergency_contact.php
+++ scripts/emergency_contact/emergency_contact.php
@@ -1,22 +1,33 @@
 <?php
-namespace APP;
+namespace APP\emergency_contact;
+
+use APP\db\connect;
+use APP\getInstance;
 
 class emergency_contact extends connect
 {
-    private $queryPost = 'INSERT INTO emergency_contact ( id_staff , cel_number, relationship, full_name, email) VALUES ( :staff_id_fk, :cel, :relationship,:name, :email)';
-    private $queryPut = 'UPDATE emergency_contact SET id_staff = :staff_id_fk, cel_number = :cel,relationship  = :relationship, full_name = :name, email = :email WHERE  id = :id';
-    private $queryGetAll = 'SELECT  id AS "id", id_staff  AS "staff_id_fk", cel_number AS "cel", relationship AS "relationship", full_name AS "name", email AS "email" FROM emergency_contact';
+    private $queryPost = 'INSERT INTO emergency_contact ( id_staff , cel_number, relationship, full_name, email) VALUES ( :staff_id_fk, :cel, :relationship,:full_name, :email)';
+    private $queryPut = 'UPDATE emergency_contact SET id_staff = :staff_id_fk, cel_number = :cel,relationship  = :relationship, full_name = :full_name, email = :email WHERE  id = :id';
+    private $queryGetAll = 'SELECT  emergency_contact.id AS "id",
+    emergency_contact.id_staff  AS "staff_id_fk",
+    staff.first_name AS "name_staff_fk",
+    emergency_contact.cel_number AS "cel",
+    emergency_contact.relationship AS "relationship",
+    emergency_contact.full_name AS "full_name",
+    emergency_contact.email AS "email"
+    FROM emergency_contact
+    INNER JOIN staff ON emergency_contact.id_staff = staff.id';
     private $queryDelete = 'DELETE FROM emergency_contact WHERE id = :id';
     private $message;
 
     use getInstance;
     //*Se definen el tipo de dato: static, private, public
-    function __construct(private $id = 1, private $id_staff = 1, private $cel_number = 1, private $relationship = 1)
+    function __construct(private $id = 1, private $id_staff = 1, private $cel_number = 1, private $relationship = 1, private $full_name = 1, private $email = 1)
     {
         parent::__construct();
 
     }
-    public function postEmergencyContact()
+    public function post_emergency_contact()
     {
         /*Prepare es literalmente preparar el query */
         $res = $this->conx->prepare($this->queryPost);
@@ -26,10 +37,8 @@ class emergency_contact extends connect
             $res->bindValue("staff_id_fk", $this->id_staff);
             $res->bindValue("cel", $this->cel_number);
             $res->bindValue("relationship", $this->relationship);
-            $res->bindValue("name", $this->full_name);
+            $res->bindValue("full_name", $this->full_name);
             $res->bindValue("email", $this->email);
-
-
             /**Execute es para ejecutar */
             $res->execute();
             $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Inserted data", "res" => $res];
@@ -42,17 +51,18 @@ class emergency_contact extends connect
         }
     }
 
-    public function updateEmergencyContact()
+    public function update_emergency_contact($id)
     {
         /*Prepare es literalmente preparar el query */
         $res = $this->conx->prepare($this->queryPut);
         /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
         try {
             /**El bindValue le asigna valores al alias que puse en el queryPut */
+            $res->bindParam("id", $id);
             $res->bindValue("staff_id_fk", $this->id_staff);
             $res->bindValue("cel", $this->cel_number);
             $res->bindValue("relationship", $this->relationship);
-            $res->bindValue("name", $this->full_name);
+            $res->bindValue("full_name", $this->full_name);
             $res->bindValue("email", $this->email);
             /**Execute es para ejecutar */
             $res->execute();
@@ -70,13 +80,13 @@ class emergency_contact extends connect
         }
     }
 
-    public function deleteEmergencyContact()
+    public function delete_emergency_contact($id)
     {
         /*Prepare es literalmente preparar el query */
         $res = $this->conx->prepare($this->queryDelete);
         /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
         try {
-            $res->bindValue("id", $this->id);
+            $res->bindParam("id", $id);
             /**Execute es para ejecutar */
             $res->execute();
 
@@ -93,7 +103,7 @@ class emergency_contact extends connect
         }
     }
 
-    public function getAllEmergencyContact()
+    public function getAll_emergency_contact()
     {
         try {
             $res = $this->conx->prepare($this->queryGetAll);
