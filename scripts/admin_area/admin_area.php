2d5983e6e4f2282950d96e2dbeba866d5b096d3f
emilynievesb <nemily1710@gmail.com>
2023-06-26 09:22:42 -0500

    feat: :sparkles: CRUD admin_area
    Se creó el respectivo CRUD de la clase, con sus respectivos inner join

--- scripts/admin_area/admin_area.php
+++ scripts/admin_area/admin_area.php
@@ -1,23 +1,39 @@
 <?php
 
-namespace APP;
+namespace APP\admin_area;
+
+use APP\db\connect;
+use APP\getInstance;
 
 class admin_area extends connect
 {
-    private $queryPost = 'INSERT INTO admin_area ( id_area, id_staff, id_position, id_journeys) VALUES ( :area_id_fk, :staff_id_fk, :position_id_fk, :journeys_id_fk)';
-    private $queryPut = 'UPDATE admin_area SET id_area = :area_id, id_staff = :staff_id_fk, id_position = :position_id_fk,id_journeys  = :journeys_id_fk WHERE  id = :id';
-    private $queryGetAll = 'SELECT  id AS "id", id_area AS "area_id_fk", id_staff AS "staff_id_fk", id_position AS "position_id_fk", id_journeys AS "journeys_id_fk" FROM admin_area';
+    private $queryPost = 'INSERT INTO admin_area (id_area, id_staff, id_position, id_journey) VALUES ( :area_id_fk, :staff_id_fk, :position_id_fk, :journeys_id_fk)';
+    private $queryPut = 'UPDATE admin_area SET id_area = :area_id, id_staff = :staff_id_fk, id_position = :position_id_fk,id_journey  = :journeys_id_fk WHERE  id = :id';
+    private $queryGetAll = 'SELECT admin_area.id AS "id",
+    admin_area.id_area AS "area_id_fk",
+    areas.name_area AS "area_name_fk",
+    admin_area.id_staff AS "staff_id_fk",
+    staff.first_name AS "first_name_staff_fk",
+    admin_area.id_position AS "position_id_fk",
+    position.name_position AS "name_position_fk",
+    admin_area.id_journey AS "journey_id_fk",
+    journey.name_journey AS "name_journey_fk"
+    FROM admin_area
+    INNER JOIN areas ON admin_area.id_area = areas.id
+    INNER JOIN staff ON admin_area.id_staff = staff.id
+    INNER JOIN position ON admin_area.id_position = position.id
+    INNER JOIN journey ON admin_area.id_journey = journey.id';
     private $queryDelete = 'DELETE FROM admin_area WHERE id = :id';
     private $message;
 
     use getInstance;
     //*Se definen el tipo de dato: static, private, public
-    function __construct(private $id = 1, private $id_area = 1, private $id_staff = 1, private $id_position = 1, private $id_journeys = 1)
+    function __construct(private $id = 1, private $id_area = 1, private $id_staff = 1, private $id_position = 1, private $id_journey = 1)
     {
         parent::__construct();
 
     }
-    public function postAdminArea()
+    public function post_admin_area()
     {
         /*Prepare es literalmente preparar el query */
         $res = $this->conx->prepare($this->queryPost);
@@ -27,7 +43,7 @@ class admin_area extends connect
             $res->bindValue("area_id_fk", $this->id_area);
             $res->bindValue("staff_id_fk", $this->id_staff);
             $res->bindValue("position_id_fk", $this->id_position);
-            $res->bindValue("journeys_id_fk", $this->id_journeys);
+            $res->bindValue("journeys_id_fk", $this->id_journey);
 
             /**Execute es para ejecutar */
             $res->execute();
@@ -41,18 +57,18 @@ class admin_area extends connect
         }
     }
 
-    public function updateAdminArea()
+    public function update_admin_area($id)
     {
         /*Prepare es literalmente preparar el query */
         $res = $this->conx->prepare($this->queryPut);
         /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
         try {
             /**El bindValue le asigna valores al alias que puse en el queryPut */
-            $res->bindValue("id", $this->id);
+            $res->bindParam("id", $id);
             $res->bindValue("area_id_fk", $this->id_area);
             $res->bindValue("staff_id_fk", $this->id_staff);
             $res->bindValue("position_id_fk", $this->id_position);
-            $res->bindValue("journeys_id_fk", $this->id_journeys);
+            $res->bindValue("journeys_id_fk", $this->id_journey);
             /**Execute es para ejecutar */
             $res->execute();
             if ($res->rowCount() > 0) {
@@ -69,13 +85,13 @@ class admin_area extends connect
         }
     }
 
-    public function deleteAdminArea()
+    public function delete_admin_area($id)
     {
         /*Prepare es literalmente preparar el query */
         $res = $this->conx->prepare($this->queryDelete);
         /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
         try {
-            $res->bindValue("id", $this->id);
+            $res->bindParam("id", $id);
             /**Execute es para ejecutar */
             $res->execute();
 
@@ -92,7 +108,7 @@ class admin_area extends connect
         }
     }
 
-    public function getAllAdminArea()
+    public function getAll_admin_area()
     {
         try {
             $res = $this->conx->prepare($this->queryGetAll);
