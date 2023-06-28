271d605053d73f6cd6b29d64e728c2a4e50f0d10
emilynievesb <nemily1710@gmail.com>
2023-06-27 23:28:18 -0500

    feat: :sparkles: CRUD tutors
    Se creó el respectivo CRUD de la clase, con sus respectivos inner join

---
+++ scripts/tutors/tutors.php
@@ -0,0 +1,147 @@
+<?php
+namespace APP\tutors;
+
+use APP\db\connect;
+use APP\getInstance;
+
+class tutors extends connect
+{
+    private $queryPost = 'INSERT INTO tutors (id_staff, id_academic_area, id_position) VALUES (:staff_fk, :academic_area_fk, :position_fk)';
+    private $queryPut = 'UPDATE tutors SET id_staff = :staff_fk, id_academic_area = :academic_area_fk, id_position = :position_fk WHERE  id = :id';
+    private $queryCampos = 'SELECT column_name FROM information_schema.columns WHERE table_name = "tutors"';
+    private $queryGetAll = 'SELECT  id AS "id",
+    tutors.id_staff AS "staff_fk",
+    tutors.id_academic_area AS "academic_area_fk",
+    tutors.id_position AS "position_fk",
+    position.name_position AS "position_name",
+    staff.first_name AS "name_staff"
+    FROM tutors
+    INNER JOIN position on tutors.id_position=position.id
+    INNER JOIN staff ON tutors.id_staff = staff.id';
+    private $queryDelete = 'DELETE FROM tutors WHERE id = :id';
+    private $message;
+
+    use getInstance;
+    //*Se definen el tipo de dato: static, private, public
+    function __construct(private $id = 1, public $id_staff = 1, private $id_academic_area = 1, private $id_position = 1)
+    {
+        parent::__construct();
+
+    }
+    public function getCampos()
+    {
+        $res = $this->conx->prepare($this->queryCampos);
+        try {
+            /**Execute es para ejecutar */
+            $res->execute();
+            $campos = array();
+            while ($row = $res->fetch(\PDO::FETCH_ASSOC)) {
+                $campos[] = $row["column_name"];
+            }
+            $this->message = $campos;
+        } catch (\PDOException $e) {
+            /**Message es un array asociativo */
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            return ($this->message);
+        }
+    }
+    public function post_tutors()
+    {
+        /*Prepare es literalmente preparar el query */
+        $res = $this->conx->prepare($this->queryPost);
+        /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
+        try {
+            /**El bindValue le asigna valores al alias que puse en el queryPost */
+            $res->bindValue("staff_fk", $this->id_staff);
+            $res->bindValue("academic_area_fk", $this->id_academic_area);
+            $res->bindValue("position_fk", $this->id_position);
+            /**Execute es para ejecutar */
+            $res->execute();
+            $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Inserted data", "res" => $res];
+        } catch (\PDOException $e) {
+            /**Message es un array asociativo */
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            echo json_encode($this->message);
+        }
+    }
+
+    public function update_tutors($id)
+    {
+        /*Prepare es literalmente preparar el query */
+        $res = $this->conx->prepare($this->queryPut);
+        /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
+        try {
+            /**El bindValue le asigna valores al alias que puse en el queryPut */
+            $res->bindParam("id", $id);
+            $res->bindValue("staff_fk", $this->id_staff);
+            $res->bindValue("academic_area_fk", $this->id_academic_area);
+            $res->bindValue("position_fk", $this->id_position);
+            /**Execute es para ejecutar */
+            $res->execute();
+            if ($res->rowCount() > 0) {
+                $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Data updated"];
+            } else {
+                $this->message = ["Code" => 404, "Message" => "Data not founded"];
+
+            }
+        } catch (\PDOException $e) {
+            /**Message es un array asociativo */if ($e->getCode() == 23000) {
+                $pattern = '/`([^`]*)`/';
+                preg_match_all($pattern, $res->errorInfo()[2], $matches);
+                $matches = array_values(array_unique($matches[count($matches) - 1]));
+                $this->message = ["Code" => $e->getCode(), "Message" => "Error, no se puede actualizar ya que el id indicado de la llave foranea  no contiene registros asociados en la tabla padre"];
+            } else {
+                $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            }
+        } finally {
+            echo json_encode($this->message);
+        }
+    }
+
+    public function delete_tutors($id)
+    {
+        /*Prepare es literalmente preparar el query */
+        $res = $this->conx->prepare($this->queryDelete);
+        /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
+        try {
+            $res->bindParam("id", $id);
+            /**Execute es para ejecutar */
+            $res->execute();
+
+            if ($res->rowCount() > 0) {
+                $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Data deleted"];
+            } else {
+                $this->message = ["Code" => 404, "Message" => "Data not founded"];
+            }
+        } catch (\PDOException $e) {
+            /**Message es un array asociativo */
+            if ($e->getCode() == 23000) {
+                $pattern = '/`([^`]*)`/';
+                preg_match_all($pattern, $res->errorInfo()[2], $matches);
+                $matches = array_values(array_unique($matches[count($matches) - 1]));
+                $this->message = ["Code" => $e->getCode(), "Message" => "Error, no se puede eliminar el id inicado ya que contiene registros asociados en la tabla $matches[1]"];
+            } else {
+                $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            }
+        } finally {
+            echo json_encode($this->message);
+        }
+    }
+
+    public function getAll_tutors()
+    {
+        try {
+            $res = $this->conx->prepare($this->queryGetAll);
+            $res->execute();
+            $campos = $this->getCampos();
+            $this->message = ["Code" => 200, "Message" => $res->fetchAll(\PDO::FETCH_ASSOC), "Campos" => $campos];
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            echo json_encode($this->message);
+        }
+    }
+}
+?>
