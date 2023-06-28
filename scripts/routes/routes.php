a811f19e1737701991e896b4add12c00a40419de
emilynievesb <nemily1710@gmail.com>
2023-06-27 23:31:46 -0500

    feat: :sparkles: CRUD ruotes
    Se creó el respectivo CRUD de la clase, con sus respectivos inner join

---
+++ scripts/routes/routes.php
@@ -0,0 +1,143 @@
+<?php
+namespace APP\routes;
+
+use APP\db\connect;
+use APP\getInstance;
+
+class routes extends connect
+{
+    private $queryPost = 'INSERT INTO routes ( name_route , start_date, end_date, description, duration_month ) VALUES ( :route, :start_date, :end_date, :description, :month)';
+    private $queryPut = 'UPDATE routes SET name_route = :route, start_date = :start_date, end_date = :end_date, description = :description, duration_month = :month WHERE  id = :id';
+    private $queryCampos = 'SELECT column_name FROM information_schema.columns WHERE table_name = "routes"';
+    private $queryGetAll = 'SELECT  id AS "id", name_route AS "route", start_date AS "start_date", end_date AS "end_date", description AS "description", duration_month AS "month" FROM routes';
+    private $queryDelete = 'DELETE FROM routes WHERE id = :id';
+    private $message;
+
+    use getInstance;
+    //*Se definen el tipo de dato: static, private, public
+    function __construct(public $id = 1, public $name_route = 1, public $start_date = 1, public $end_date = 1, public $description = 1, public $duration_month = 1)
+    {
+        parent::__construct();
+
+    }
+    public function getCampos()
+    {
+        $res = $this->conx->prepare($this->queryCampos);
+        try {
+            /**Execute es para ejecutar */
+            $res->execute();
+            $campos = array();
+            while ($row = $res->fetch(\PDO::FETCH_ASSOC)) {
+                $campos[] = $row["column_name"];
+            }
+            $this->message = $campos;
+        } catch (\PDOException $e) {
+            /**Message es un array asociativo */
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            return ($this->message);
+        }
+    }
+    public function post_routes()
+    {
+        /*Prepare es literalmente preparar el query */
+        $res = $this->conx->prepare($this->queryPost);
+        /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
+        try {
+            /**El bindValue le asigna valores al alias que puse en el queryPost */
+            $res->bindValue("route", $this->name_route);
+            $res->bindValue("start_date", $this->start_date);
+            $res->bindValue("end_date", $this->end_date);
+            $res->bindValue("description", $this->description);
+            $res->bindValue("month", $this->duration_month);
+            /**Execute es para ejecutar */
+            $res->execute();
+            $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Inserted data", "res" => $res];
+        } catch (\PDOException $e) {
+            /**Message es un array asociativo */
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            echo json_encode($this->message);
+        }
+    }
+
+    public function update_routes()
+    {
+        /*Prepare es literalmente preparar el query */
+        $res = $this->conx->prepare($this->queryPut);
+        /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
+        try {
+            /**El bindValue le asigna valores al alias que puse en el queryPut */
+            $res->bindValue("id", $this->id);
+            $res->bindValue("route", $this->name_route);
+            $res->bindValue("start_date", $this->start_date);
+            $res->bindValue("end_date", $this->end_date);
+            $res->bindValue("description", $this->description);
+            $res->bindValue("month", $this->duration_month);
+            /**Execute es para ejecutar */
+            $res->execute();
+            if ($res->rowCount() > 0) {
+                $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Data updated"];
+            } else {
+                $this->message = ["Code" => 404, "Message" => "Data not founded"];
+
+            }
+        } catch (\PDOException $e) {
+            /**Message es un array asociativo */if ($e->getCode() == 23000) {
+                $pattern = '/`([^`]*)`/';
+                preg_match_all($pattern, $res->errorInfo()[2], $matches);
+                $matches = array_values(array_unique($matches[count($matches) - 1]));
+                $this->message = ["Code" => $e->getCode(), "Message" => "Error, no se puede actualizar ya que el id indicado de la llave foranea  no contiene registros asociados en la tabla padre"];
+            } else {
+                $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            }
+        } finally {
+            echo json_encode($this->message);
+        }
+    }
+
+    public function delete_routes()
+    {
+        /*Prepare es literalmente preparar el query */
+        $res = $this->conx->prepare($this->queryDelete);
+        /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
+        try {
+            $res->bindValue("id", $this->id);
+            /**Execute es para ejecutar */
+            $res->execute();
+
+            if ($res->rowCount() > 0) {
+                $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Data deleted"];
+            } else {
+                $this->message = ["Code" => 404, "Message" => "Data not founded"];
+            }
+        } catch (\PDOException $e) {
+            /**Message es un array asociativo */
+            if ($e->getCode() == 23000) {
+                $pattern = '/`([^`]*)`/';
+                preg_match_all($pattern, $res->errorInfo()[2], $matches);
+                $matches = array_values(array_unique($matches[count($matches) - 1]));
+                $this->message = ["Code" => $e->getCode(), "Message" => "Error, no se puede eliminar el id inicado ya que contiene registros asociados en la tabla $matches[1]"];
+            } else {
+                $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            }
+        } finally {
+            echo json_encode($this->message);
+        }
+    }
+
+    public function getAll_routes()
+    {
+        try {
+            $res = $this->conx->prepare($this->queryGetAll);
+            $res->execute();
+            $campos = $this->getCampos();
+            $this->message = ["Code" => 200, "Message" => $res->fetchAll(\PDO::FETCH_ASSOC), "Campos" => $campos];
+        } catch (\PDOException $e) {
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            echo json_encode($this->message);
+        }
+    }
+}
+?>
