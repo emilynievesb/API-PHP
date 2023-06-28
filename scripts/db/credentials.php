713f5c8e94417ae992e93c2662317af1942be5bc
emilynievesb <nemily1710@gmail.com>
2023-06-27 23:38:21 -0500

    feat: :sparkles: Namespace
    Se agregó el namespace para el uso del autoload de composer

--- scripts/db/credentials.php
+++ scripts/db/credentials.php
@@ -1,16 +1,16 @@
 <?php
 // Si hay un atributo privado, solo se puede usar en la clase donde está, la herencia no puede acceder a él.
-namespace APP;
+namespace APP\db;
 
 abstract class credentials
 {
-    // protected $host = 'localhost';
-    protected $host = '172.16.48.204';
+    protected $host = 'localhost';
+    // protected $host = '172.16.49.20';
 
-    private $user = 'sputnik';
-    // private $user = 'root';
-    private $password = 'Sp3tn1kC@';
-    // private $password = '';
+    // private $user = 'sputnik';
+    private $user = 'root';
+    // private $password = 'Sp3tn1kC@';
+    private $password = '';
 
     protected $dbname = 'campusland';
 
