23ed5435673ccd8c66c07377a2a870b931352f8c
emilynievesb <nemily1710@gmail.com>
2023-07-01 19:12:15 -0500

    feat: :sparkles: CRUD campers
    Se creó el respectivo CRUD de la clase, con sus respectivos inner join

--- scripts/campers/campers.php
+++ scripts/campers/campers.php
@@ -6,9 +6,28 @@ use APP\getInstance;
 
 class campers extends connect
 {
-    private $queryPost = 'INSERT INTO campers (id_team_schedule, id_route, id_trainer,id_psycologist, id_teacher, id_level, id_journey,id_staff) VALUES (:team_fk, :route_fk, :trainer_fk, :psycologist_fk, :teacher_fk, :level_fk, :journey_fk, :staff_fk)';
-    private $queryPut = 'UPDATE campers SET id_team_schedule = :team_fk, id_route = :route_fk, id_trainer = :trainer_fk, id_psycologist = :psycologist_fk, id_teacher = :teacher_fk, id_level = :level_fk, id_journey = :journey_fk, id_staff = :staff_fk WHERE  id = :id';
-    private $queryGetAll = 'SELECT  id AS "id", id_team_schedule AS "team_fk", id_route AS "route_fk",id_trainer AS "trainer_fk",id_psycologist AS "psycologist_fk",id_teacher AS "teacher_fk",id_level AS "level_fk",id_journey AS "journey_fk",id_staff AS "staff_fk", FROM campers';
+    private $queryPost = 'INSERT INTO campers (id_team_schedule, id_route, id_trainer,id_psycologist, id_teacher, id_level, id_journey,id_staff) VALUES (:schedule_fk, :route_fk, :trainer_fk, :psycologist_fk, :teacher_fk, :level_fk, :journey_fk, :staff_fk)';
+    private $queryPut = 'UPDATE campers SET id_team_schedule = :schedule_fk, id_route = :route_fk, id_trainer = :trainer_fk, id_psycologist = :psycologist_fk, id_teacher = :teacher_fk, id_level = :level_fk, id_journey = :journey_fk, id_staff = :staff_fk WHERE  id = :id';
+    private $queryCampos = 'SELECT column_name FROM information_schema.columns WHERE table_name = "campers" AND table_schema = "campusland";
+    ';
+    private $queryGetAll = 'SELECT  campers.id AS "id",
+    campers.id_team_schedule AS "schedule_fk",
+    campers.id_route AS "route_fk",
+    campers.id_trainer AS "trainer_fk",
+    campers.id_psycologist AS "psycologist_fk",
+    campers.id_teacher AS "teacher_fk",
+    campers.id_level AS "level_fk",
+    campers.id_journey AS "journey_fk",
+    campers.id_staff AS "staff_fk",
+    team_schedule.team_name AS "team_name_fk",
+    routes.name_route AS "name_route_fk",
+    levels.name_level AS "name_level_fk",
+    journey.name_journey AS "name_journey_fk"
+    FROM campers
+    INNER JOIN team_schedule ON campers.id_team_schedule = team_schedule.id
+    INNER JOIN routes ON campers.id_route = routes.id
+    INNER JOIN levels ON campers.id_level = levels.id
+    INNER JOIN journey ON campers.id_journey = journey.id ';
     private $queryDelete = 'DELETE FROM campers WHERE id = :id';
     private $message;
 
@@ -19,6 +38,24 @@ class campers extends connect
         parent::__construct();
 
     }
+    public function getCampos()
+    {
+        $res = $this->conx->prepare($this->queryCampos);
+        try {
+            /**Execute es para ejecutar */
+            $res->execute();
+            $campos = array();
+            while ($row = $res->fetch(\PDO::FETCH_ASSOC)) {
+                $campos[] = $row["column_name"];
+            }
+            $this->message = $campos;
+        } catch (\PDOException $e) {
+            /**Message es un array asociativo */
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            return ($this->message);
+        }
+    }
     public function post_campers()
     {
         /*Prepare es literalmente preparar el query */
@@ -26,37 +63,47 @@ class campers extends connect
         /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
         try {
             /**El bindValue le asigna valores al alias que puse en el queryPost */
-            $res->bindValue("team_fk", $this->id_team_schedule);
+            $res->bindValue("schedule_fk", $this->id_team_schedule);
             $res->bindValue("route_fk", $this->id_route);
             $res->bindValue("trainer_fk", $this->id_trainer);
             $res->bindValue("psycologist_fk", $this->id_psycologist);
             $res->bindValue("teacher_fk", $this->id_teacher);
-            $res->bindValue("level_fk", $this->id_staff);
+            $res->bindValue("level_fk", $this->id_level);
+            $res->bindValue("journey_fk", $this->id_journey);
+            $res->bindValue("staff_fk", $this->id_staff);
             /**Execute es para ejecutar */
             $res->execute();
             $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Inserted data", "res" => $res];
         } catch (\PDOException $e) {
-            /**Message es un array asociativo */
-            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            /**Message es un array asociativo */if ($e->getCode() == 23000) {
+                $pattern = '/`([^`]*)`/';
+                preg_match_all($pattern, $res->errorInfo()[2], $matches);
+                $matches = array_values(array_unique($matches[count($matches) - 1]));
+                $this->message = ["Code" => $e->getCode(), "Message" => "Error, no se puede actualizar ya que el id indicado de la llave foranea no contiene registros asociados en la tabla $matches[4]", $matches];
+            } else {
+                $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            }
         } finally {
-            print_r($this->message);
+            echo json_encode($this->message);
         }
     }
 
-    public function update_campers()
+    public function update_campers($id)
     {
         /*Prepare es literalmente preparar el query */
         $res = $this->conx->prepare($this->queryPut);
         /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
         try {
             /**El bindValue le asigna valores al alias que puse en el queryPut */
-            $res->bindValue("id", $this->id);
-            $res->bindValue("team_fk", $this->id_team_schedule);
+            $res->bindParam("id", $id);
+            $res->bindValue("schedule_fk", $this->id_team_schedule);
             $res->bindValue("route_fk", $this->id_route);
             $res->bindValue("trainer_fk", $this->id_trainer);
             $res->bindValue("psycologist_fk", $this->id_psycologist);
             $res->bindValue("teacher_fk", $this->id_teacher);
-            $res->bindValue("level_fk", $this->id_staff);
+            $res->bindValue("level_fk", $this->id_level);
+            $res->bindValue("journey_fk", $this->id_journey);
+            $res->bindValue("staff_fk", $this->id_staff);
             /**Execute es para ejecutar */
             $res->execute();
             if ($res->rowCount() > 0) {
@@ -67,19 +114,26 @@ class campers extends connect
             }
         } catch (\PDOException $e) {
             /**Message es un array asociativo */
-            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            if ($e->getCode() == 23000) {
+                $pattern = '/`([^`]*)`/';
+                preg_match_all($pattern, $res->errorInfo()[2], $matches);
+                $matches = array_values(array_unique($matches[count($matches) - 1]));
+                $this->message = ["Code" => $e->getCode(), "Message" => "Error, no se puede actualizar ya que el id indicado de la llave foranea  no contiene registros asociados en la tabla $matches[4]"];
+            } else {
+                $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            }
         } finally {
-            print_r($this->message);
+            echo json_encode($this->message);
         }
     }
 
-    public function delete_campers()
+    public function delete_campers($id)
     {
         /*Prepare es literalmente preparar el query */
         $res = $this->conx->prepare($this->queryDelete);
         /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
         try {
-            $res->bindValue("id", $this->id);
+            $res->bindParam("id", $id);
             /**Execute es para ejecutar */
             $res->execute();
 
@@ -94,12 +148,12 @@ class campers extends connect
                 $pattern = '/`([^`]*)`/';
                 preg_match_all($pattern, $res->errorInfo()[2], $matches);
                 $matches = array_values(array_unique($matches[count($matches) - 1]));
-                $mensaje = ["Code" => $e->getCode(), "Message" => "Error, no se puede eliminar el id inicado ya que contiene registros asociados en la tabla $matches[1]"];
+                $this->message = ["Code" => $e->getCode(), "Message" => "Error, no se puede eliminar el id inicado ya que contiene registros asociados en la tabla $matches[1]"];
             } else {
-                $mensaje = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+                $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
             }
         } finally {
-            print_r($mensaje);
+            echo json_encode($this->message);
         }
     }
 
@@ -108,11 +162,12 @@ class campers extends connect
         try {
             $res = $this->conx->prepare($this->queryGetAll);
             $res->execute();
-            $this->message = ["Code" => 200, "Message" => $res->fetchAll(\PDO::FETCH_ASSOC)];
+            $campos = $this->getCampos();
+            $this->message = ["Code" => 200, "Message" => $res->fetchAll(\PDO::FETCH_ASSOC), "Campos" => $campos];
         } catch (\PDOException $e) {
             $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
         } finally {
-            print_r($this->message);
+            echo json_encode($this->message);
         }
     }
 }
