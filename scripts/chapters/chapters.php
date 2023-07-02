a931dabfd435ed53b2b8b9e2a37e4c4a9d6f4690
emilynievesb <nemily1710@gmail.com>
2023-07-01 21:28:40 -0500

    feat: :sparkles: CRUD chapters
    Se creó el respectivo CRUD de la clase, con sus respectivos inner join

--- scripts/chapters/chapters.php
+++ scripts/chapters/chapters.php
@@ -6,17 +6,45 @@ use APP\getInstance;
 
 class chapters extends connect
 {
-    private $queryPost = 'INSERT INTO chapters (id_thematic_units, 	name_chapter, start_date, description, duration_days) VALUES (:thematics_fk, :chapter, :date, :description, :duration_days)';
-    private $queryPut = 'UPDATE chapters SET id_thematic_units = :thematics_fk, name_chapter = :chapter, start_date = :date, description = :description, duration_days = :duration_days WHERE  id = :id';
-    private $queryGetAll = 'SELECT  id AS "id", id_thematic_units AS "thematics_fk", name_chapter AS "chapter", start_date AS "date", description AS "description", duration_days AS "duration_days" FROM chapters';
+    private $queryPost = 'INSERT INTO chapters (id_thematic_units, name_chapter, start_date,end_date, description, duration_days) VALUES (:thematics_fk, :chapter, :date, :end_date,:description, :duration_days)';
+    private $queryPut = 'UPDATE chapters SET id_thematic_units = :thematics_fk, name_chapter = :chapter, start_date = :date, end_date = :end_date, description = :description, duration_days = :duration_days WHERE  id = :id';
+    private $queryCampos = 'SELECT column_name FROM information_schema.columns WHERE table_name = "chapters" AND table_schema = "campusland"';
+    private $queryGetAll = 'SELECT chapters.id AS "id",
+    chapters.id_thematic_units AS "thematics_fk",
+    chapters.name_chapter AS "chapter",
+    chapters.start_date AS "start_date",
+    chapters.end_date AS "end_date",
+    chapters.description AS "description",
+    chapters.duration_days AS "duration_days",
+    thematic_units.name_thematics_units AS "name_units_fk"
+    FROM chapters
+    INNER JOIN thematic_units ON chapters.id_thematic_units = thematic_units.id';
     private $queryDelete = 'DELETE FROM chapters WHERE id = :id';
     private $message;
 
     use getInstance;
     //*Se definen el tipo de dato: static, private, public
-    function __construct(private $id = 1, private $id_thematic_units = 1, public $name_chapter = 1, public $start_date = 1, public $description = 1, public $duration_days = 1)
+    function __construct(private $id = 1, private $id_thematic_units = 1, public $name_chapter = 1, public $start_date = 1, public $end_date = 1, public $description = 1, public $duration_days = 1)
     {
         parent::__construct();
+    }
+    public function getCampos()
+    {
+        $res = $this->conx->prepare($this->queryCampos);
+        try {
+            /**Execute es para ejecutar */
+            $res->execute();
+            $campos = array();
+            while ($row = $res->fetch(\PDO::FETCH_ASSOC)) {
+                $campos[] = $row["column_name"];
+            }
+            $this->message = $campos;
+        } catch (\PDOException $e) {
+            /**Message es un array asociativo */
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            return ($this->message);
+        }
     }
     public function post_chapters()
     {
@@ -28,30 +56,38 @@ class chapters extends connect
             $res->bindValue("thematics_fk", $this->id_thematic_units);
             $res->bindValue("chapter", $this->name_chapter);
             $res->bindValue("date", $this->start_date);
+            $res->bindValue("end_date", $this->end_date);
             $res->bindValue("description", $this->description);
             $res->bindValue("duration_days", $this->duration_days);
             /**Execute es para ejecutar */
             $res->execute();
             $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Inserted data", "res" => $res];
         } catch (\PDOException $e) {
-            /**Message es un array asociativo */
-            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            /**Message es un array asociativo */if ($e->getCode() == 23000) {
+                $pattern = '/`([^`]*)`/';
+                preg_match_all($pattern, $res->errorInfo()[2], $matches);
+                $matches = array_values(array_unique($matches[count($matches) - 1]));
+                $this->message = ["Code" => $e->getCode(), "Message" => "Error, no se puede actualizar ya que el id indicado de la llave foranea no contiene registros asociados en la tabla $matches[4]", $matches];
+            } else {
+                $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            }
         } finally {
-            print_r($this->message);
+            echo json_encode($this->message);
         }
     }
 
-    public function update_chapters()
+    public function update_chapters($id)
     {
         /*Prepare es literalmente preparar el query */
         $res = $this->conx->prepare($this->queryPut);
         /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
         try {
             /**El bindValue le asigna valores al alias que puse en el queryPut */
-            $res->bindValue("id", $this->id);
+            $res->bindParam("id", $id);
             $res->bindValue("thematics_fk", $this->id_thematic_units);
             $res->bindValue("chapter", $this->name_chapter);
             $res->bindValue("date", $this->start_date);
+            $res->bindValue("end_date", $this->end_date);
             $res->bindValue("description", $this->description);
             $res->bindValue("duration_days", $this->duration_days);
             /**Execute es para ejecutar */
@@ -60,23 +96,29 @@ class chapters extends connect
                 $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Data updated"];
             } else {
                 $this->message = ["Code" => 404, "Message" => "Data not founded"];
-
             }
         } catch (\PDOException $e) {
             /**Message es un array asociativo */
-            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            if ($e->getCode() == 23000) {
+                $pattern = '/`([^`]*)`/';
+                preg_match_all($pattern, $res->errorInfo()[2], $matches);
+                $matches = array_values(array_unique($matches[count($matches) - 1]));
+                $this->message = ["Code" => $e->getCode(), "Message" => "Error, no se puede actualizar ya que el id indicado de la llave foranea  no contiene registros asociados en la tabla $matches[4]"];
+            } else {
+                $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            }
         } finally {
-            print_r($this->message);
+            echo json_encode($this->message);
         }
     }
 
-    public function delete_chapters()
+    public function delete_chapters($id)
     {
         /*Prepare es literalmente preparar el query */
         $res = $this->conx->prepare($this->queryDelete);
         /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
         try {
-            $res->bindValue("id", $this->id);
+            $res->bindParam("id", $id);
             /**Execute es para ejecutar */
             $res->execute();
 
@@ -87,9 +129,16 @@ class chapters extends connect
             }
         } catch (\PDOException $e) {
             /**Message es un array asociativo */
-            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            if ($e->getCode() == 23000) {
+                $pattern = '/`([^`]*)`/';
+                preg_match_all($pattern, $res->errorInfo()[2], $matches);
+                $matches = array_values(array_unique($matches[count($matches) - 1]));
+                $this->message = ["Code" => $e->getCode(), "Message" => "Error, no se puede eliminar el id inicado ya que contiene registros asociados en la tabla $matches[1]"];
+            } else {
+                $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            }
         } finally {
-            print_r($this->message);
+            echo json_encode($this->message);
         }
     }
 
@@ -98,11 +147,12 @@ class chapters extends connect
         try {
             $res = $this->conx->prepare($this->queryGetAll);
             $res->execute();
-            $this->message = ["Code" => 200, "Message" => $res->fetchAll(\PDO::FETCH_ASSOC)];
+            $campos = $this->getCampos();
+            $this->message = ["Code" => 200, "Message" => $res->fetchAll(\PDO::FETCH_ASSOC), "Campos" => $campos];
         } catch (\PDOException $e) {
             $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
         } finally {
-            print_r($this->message);
+            echo json_encode($this->message);
         }
     }
 }
