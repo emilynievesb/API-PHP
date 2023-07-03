aa072100a4c32cf4c19d1a2710286d9b482b9a93
emilynievesb <nemily1710@gmail.com>
2023-07-03 16:05:44 -0500

    feat: :sparkles: CRUD software skills
    Se creó el respectivo CRUD de la clase, con sus respectivos inner join

--- scripts/software_skills/software_skills.php
+++ scripts/software_skills/software_skills.php
@@ -7,8 +7,21 @@ use APP\getInstance;
 class software_skills extends connect
 {
     private $queryPost = 'INSERT INTO software_skills ( id_team_schedule , id_journey, id_trainer, id_location, id_subject) VALUES ( :schedule_id_fk, :journey_fk, :trainer_fk,:location_fk, :subject_fk)';
-    private $queryPut = 'UPDATE software_skills SET id_team_schedule = :schedule_id_fk, id_journey = :journey_fk, id_trainer_fk  = :trainer_fk, id_location = :location_fk, id_subject = :subject_fk WHERE  id = :id';
-    private $queryGetAll = 'SELECT  id AS "id", id_team_schedule  AS "schedule_id_fk", id_journey AS "journey_fk", id_trainer AS "trainer_fk", id_location AS "location_fk", id_subject AS "subject_fk" FROM software_skills';
+    private $queryPut = 'UPDATE software_skills SET id_team_schedule = :schedule_id_fk, id_journey = :journey_fk, id_trainer  = :trainer_fk, id_location = :location_fk, id_subject = :subject_fk WHERE  id = :id';
+    private $queryCampos = 'SELECT column_name FROM information_schema.columns WHERE table_name = "software_skills" AND table_schema = "campusland"';
+    private $queryGetAll = 'SELECT software_skills.id AS "id",
+    software_skills.id_team_schedule AS "schedule_id_fk",
+    software_skills.id_journey AS "journey_fk",
+    software_skills.id_trainer AS "trainer_fk",
+    software_skills.id_location AS "location_fk",
+    software_skills.id_subject AS "subject_fk",
+    journey.name_journey AS "name_journey_fk",
+    subjects.name_subject AS "subject_name_fk",
+    locations.name_location AS "location_name_fk"
+    FROM software_skills
+    INNER JOIN journey ON software_skills.id_journey = journey.id
+    INNER JOIN locations ON software_skills.id_location = locations.id
+    INNER JOIN subjects ON software_skills.id_subject = subjects.id';
     private $queryDelete = 'DELETE FROM software_skills WHERE id = :id';
     private $message;
 
@@ -18,6 +31,24 @@ class software_skills extends connect
     {
         parent::__construct();
 
+    }
+    public function getCampos()
+    {
+        $res = $this->conx->prepare($this->queryCampos);
+        try {
+            /**Execute es para ejecutar */
+            $res->execute();
+            $campos = array();
+            while ($row = $res->fetch(\PDO::FETCH_ASSOC)) {
+                $campos[] = $row["column_name"];
+            }
+            $this->message = $campos;
+        } catch (\PDOException $e) {
+            /**Message es un array asociativo */
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            return ($this->message);
+        }
     }
     public function post_software_skills()
     {
@@ -35,22 +66,27 @@ class software_skills extends connect
             $res->execute();
             $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Inserted data", "res" => $res];
         } catch (\PDOException $e) {
-            /**Message es un array asociativo */
-            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
-
+            /**Message es un array asociativo */if ($e->getCode() == 23000) {
+                $pattern = '/`([^`]*)`/';
+                preg_match_all($pattern, $res->errorInfo()[2], $matches);
+                $matches = array_values(array_unique($matches[count($matches) - 1]));
+                $this->message = ["Code" => $e->getCode(), "Message" => "Error, no se puede actualizar ya que el id indicado de la llave foranea no contiene registros asociados en la tabla $matches[4]", $matches];
+            } else {
+                $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            }
         } finally {
-            print_r($this->message);
+            echo json_encode($this->message);
         }
     }
 
-    public function update_software_skills()
+    public function update_software_skills($id)
     {
         /*Prepare es literalmente preparar el query */
         $res = $this->conx->prepare($this->queryPut);
         /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
         try {
             /**El bindValue le asigna valores al alias que puse en el queryPut */
-            $res->bindValue("id", $this->id);
+            $res->bindParam("id", $id);
             $res->bindValue("schedule_id_fk", $this->id_team_schedule);
             $res->bindValue("journey_fk", $this->id_journey);
             $res->bindValue("trainer_fk", $this->id_trainer);
@@ -62,26 +98,31 @@ class software_skills extends connect
                 $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Data updated"];
             } else {
                 $this->message = ["Code" => 404, "Message" => "Data not founded"];
-
             }
         } catch (\PDOException $e) {
             /**Message es un array asociativo */
-            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            if ($e->getCode() == 23000) {
+                $pattern = '/`([^`]*)`/';
+                preg_match_all($pattern, $res->errorInfo()[2], $matches);
+                $matches = array_values(array_unique($matches[count($matches) - 1]));
+                $this->message = ["Code" => $e->getCode(), "Message" => "Error, no se puede actualizar ya que el id indicado de la llave foranea  no contiene registros asociados en la tabla $matches[4]"];
+            } else {
+                $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            }
         } finally {
-            print_r($this->message);
+            echo json_encode($this->message);
         }
     }
 
-    public function delete_software_skills()
+    public function delete_software_skills($id)
     {
         /*Prepare es literalmente preparar el query */
         $res = $this->conx->prepare($this->queryDelete);
         /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
         try {
-            $res->bindValue("id", $this->id);
+            $res->bindParam("id", $id);
             /**Execute es para ejecutar */
             $res->execute();
-
             if ($res->rowCount() > 0) {
                 $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Data deleted"];
             } else {
@@ -89,9 +130,16 @@ class software_skills extends connect
             }
         } catch (\PDOException $e) {
             /**Message es un array asociativo */
-            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            if ($e->getCode() == 23000) {
+                $pattern = '/`([^`]*)`/';
+                preg_match_all($pattern, $res->errorInfo()[2], $matches);
+                $matches = array_values(array_unique($matches[count($matches) - 1]));
+                $this->message = ["Code" => $e->getCode(), "Message" => "Error, no se puede eliminar el id inicado ya que contiene registros asociados en la tabla $matches[1]"];
+            } else {
+                $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            }
         } finally {
-            print_r($this->message);
+            echo json_encode($this->message);
         }
     }
 
@@ -100,11 +148,12 @@ class software_skills extends connect
         try {
             $res = $this->conx->prepare($this->queryGetAll);
             $res->execute();
-            $this->message = ["Code" => 200, "Message" => $res->fetchAll(\PDO::FETCH_ASSOC)];
+            $campos = $this->getCampos();
+            $this->message = ["Code" => 200, "Message" => $res->fetchAll(\PDO::FETCH_ASSOC), "Campos" => $campos];
         } catch (\PDOException $e) {
             $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
         } finally {
-            print_r($this->message);
+            echo json_encode($this->message);
         }
     }
 }
