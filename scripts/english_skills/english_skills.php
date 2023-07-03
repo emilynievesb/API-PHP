31cd7240cc39b2bc8dda13e98c4ca33f50df6746
emilynievesb <nemily1710@gmail.com>
2023-07-03 14:38:02 -0500

    feat: :sparkles: CRUD english skills
    Se creó el respectivo CRUD de la clase, con sus respectivos inner join

--- scripts/english_skills/english_skills.php
+++ scripts/english_skills/english_skills.php
@@ -8,7 +8,20 @@ class english_skills extends connect
 {
     private $queryPost = 'INSERT INTO english_skills ( id_team_schedule , id_journey, id_teacher, id_location, id_subject) VALUES ( :schedule_id_fk, :journey_fk, :teacher_fk,:location_fk, :subject_fk )';
     private $queryPut = 'UPDATE english_skills SET id_team_schedule = :schedule_id_fk, id_journey = :journey_fk, id_teacher  = :teacher_fk, id_location = :location_fk, id_subject = :subject_fk WHERE  id = :id';
-    private $queryGetAll = 'SELECT  id AS "id", id_team_schedule  AS "schedule_id_fk", id_journey AS "journey_fk", id_teacher AS "teacher_fk", id_location AS "location_fk", id_subject AS "subject_fk" FROM english_skills';
+    private $queryCampos = 'SELECT column_name FROM information_schema.columns WHERE table_name = "english_skills" AND table_schema = "campusland"';
+    private $queryGetAll = 'SELECT english_skills.id AS "id",
+    english_skills.id_team_schedule AS "schedule_id_fk",
+    english_skills.id_journey AS "journey_fk",
+    english_skills.id_teacher AS "teacher_fk",
+    english_skills.id_location AS "location_fk",
+    english_skills.id_subject AS "subject_fk",
+    journey.name_journey AS "name_journey_fk",
+    locations.name_location AS "location_name_fk",
+    subjects.name_subject AS "subject_name_fk"
+    FROM english_skills
+    INNER JOIN journey ON english_skills.id_journey = journey.id
+    INNER JOIN locations ON english_skills.id_location = locations.id
+    INNER JOIN subjects ON english_skills.id_subject = subjects.id';
     private $queryDelete = 'DELETE FROM english_skills WHERE id = :id';
     private $message;
 
@@ -18,6 +31,24 @@ class english_skills extends connect
     {
         parent::__construct();
 
+    }
+    public function getCampos()
+    {
+        $res = $this->conx->prepare($this->queryCampos);
+        try {
+            /**Execute es para ejecutar */
+            $res->execute();
+            $campos = array();
+            while ($row = $res->fetch(\PDO::FETCH_ASSOC)) {
+                $campos[] = $row["column_name"];
+            }
+            $this->message = $campos;
+        } catch (\PDOException $e) {
+            /**Message es un array asociativo */
+            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+        } finally {
+            return ($this->message);
+        }
     }
     public function post_english_skills()
     {
@@ -35,22 +66,28 @@ class english_skills extends connect
             $res->execute();
             $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Inserted data", "res" => $res];
         } catch (\PDOException $e) {
-            /**Message es un array asociativo */
-            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
-
+            /**Message es un array asociativo */if ($e->getCode() == 23000) {
+                $pattern = '/`([^`]*)`/';
+                preg_match_all($pattern, $res->errorInfo()[2], $matches);
+                $matches = array_values(array_unique($matches[count($matches) - 1]));
+                $this->message = ["Code" => $e->getCode(), "Message" => "Error, no se puede actualizar ya que el id indicado de la llave foranea no contiene registros asociados en la tabla $matches[4]", $matches];
+            } else {
+                $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            }
         } finally {
-            print_r($this->message);
+            echo json_encode($this->message);
         }
     }
 
-    public function update_english_skills()
+
+    public function update_english_skills($id)
     {
         /*Prepare es literalmente preparar el query */
         $res = $this->conx->prepare($this->queryPut);
         /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
         try {
             /**El bindValue le asigna valores al alias que puse en el queryPut */
-            $res->bindValue("id", $this->id);
+            $res->bindParam("id", $id);
             $res->bindValue("schedule_id_fk", $this->id_team_schedule);
             $res->bindValue("journey_fk", $this->id_journey);
             $res->bindValue("teacher_fk", $this->id_teacher);
@@ -66,22 +103,28 @@ class english_skills extends connect
             }
         } catch (\PDOException $e) {
             /**Message es un array asociativo */
-            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            if ($e->getCode() == 23000) {
+                $pattern = '/`([^`]*)`/';
+                preg_match_all($pattern, $res->errorInfo()[2], $matches);
+                $matches = array_values(array_unique($matches[count($matches) - 1]));
+                $this->message = ["Code" => $e->getCode(), "Message" => "Error, no se puede actualizar ya que el id indicado de la llave foranea  no contiene registros asociados en la tabla $matches[4]"];
+            } else {
+                $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            }
         } finally {
-            print_r($this->message);
+            echo json_encode($this->message);
         }
     }
 
-    public function delete_english_skills()
+    public function delete_english_skills($id)
     {
         /*Prepare es literalmente preparar el query */
         $res = $this->conx->prepare($this->queryDelete);
         /**Todas las solicitudes, así sea un connect deben intentarse dentro de un try-catch */
         try {
-            $res->bindValue("id", $this->id);
+            $res->bindParam("id", $id);
             /**Execute es para ejecutar */
             $res->execute();
-
             if ($res->rowCount() > 0) {
                 $this->message = ["Code" => 200 + $res->rowCount(), "Message" => "Data deleted"];
             } else {
@@ -89,9 +132,16 @@ class english_skills extends connect
             }
         } catch (\PDOException $e) {
             /**Message es un array asociativo */
-            $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            if ($e->getCode() == 23000) {
+                $pattern = '/`([^`]*)`/';
+                preg_match_all($pattern, $res->errorInfo()[2], $matches);
+                $matches = array_values(array_unique($matches[count($matches) - 1]));
+                $this->message = ["Code" => $e->getCode(), "Message" => "Error, no se puede eliminar el id inicado ya que contiene registros asociados en la tabla $matches[1]"];
+            } else {
+                $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
+            }
         } finally {
-            print_r($this->message);
+            echo json_encode($this->message);
         }
     }
 
@@ -100,11 +150,12 @@ class english_skills extends connect
         try {
             $res = $this->conx->prepare($this->queryGetAll);
             $res->execute();
-            $this->message = ["Code" => 200, "Message" => $res->fetchAll(\PDO::FETCH_ASSOC)];
+            $campos = $this->getCampos();
+            $this->message = ["Code" => 200, "Message" => $res->fetchAll(\PDO::FETCH_ASSOC), "Campos" => $campos];
         } catch (\PDOException $e) {
             $this->message = ["Code" => $e->getCode(), "Message" => $res->errorInfo()[2]];
         } finally {
-            print_r($this->message);
+            echo json_encode($this->message);
         }
     }
 }
